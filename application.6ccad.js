9a28db7c3c46d47b8d3ec3ff8d14930671ec016e
Hua Zan Ming <phamquangminhex@gmail.com>
2023-12-01 00:58:21 +0700

    - Update build

--- application.6ccad.js
+++ application.6ccad.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
 
     function loadSettingsJson(cc) {
       var server = '';
-      var settings = 'src/settings.528e4.json';
+      var settings = 'src/settings.0ad85.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
--- index.0afd5.js
+++ index.0afd5.js
@@ -1,4 +1,4 @@
-System.register(["./application.1a247.js"], function (_export, _context) {
+System.register(["./application.6ccad.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -85,8 +85,8 @@ System.register(["./application.1a247.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application1a247Js) {
-      createApplication = _application1a247Js.createApplication;
+    setters: [function (_application6ccadJs) {
+      createApplication = _application6ccadJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
